bed1fdcc22c242c26b0d983cedafe0c2e7f2a5dd
Dmitriy Fateev <dmitrijfateev82@gmail.com>
2026-07-19 15:43:16 +0700

    updated file

--- homework/dmitriy_fateev/Lesson_4/homework.py
+++ homework/dmitriy_fateev/Lesson_4/homework.py
@@ -1,6 +1,6 @@
-my_dict = {'tuple': (1, 2, 3, 4, 5),'list' : [1, 2, 3, 4, 5],
-           'dict': {'first_key': 'first_meaning', 'second_key': 2, 'third_key': 3.0, 'fourth_key': 4, 5: 'fifth'},
-           'set':{1, 'banana', 3.7, 4000, 5}}
+my_dict = {'tuple': (1, 2, 3, 4, 5), 'list' : [1, 2, 3, 4, 5],
+           'dict': {'first_key': 'first_meaning', 'second_key': 2, 'third_key': 3.0, 'fourth_key': 4, 5:'fifth'},
+           'set': {1, 'banana', 3.7, 4000, 5}}
 
 
 my_dict['list'].append(6)
