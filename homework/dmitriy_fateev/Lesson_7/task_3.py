4a6de3d79b425bd20afc7c2d83bb1ccc96056fb9
Dmitriy Fateev <dmitrijfateev82@gmail.com>
2026-07-22 19:02:00 +0700

    added newlines for requirements

--- homework/dmitriy_fateev/Lesson_7/task_3.py
+++ homework/dmitriy_fateev/Lesson_7/task_3.py
@@ -2,10 +2,12 @@ first_program_output = "результат операции: 42"
 second_program_output = "результат операции: 514"
 third_program_output = "результат работы программы: 9"
 
+
 def result(text):
     index = text.index(':')
     number = int(text[index + 1:]) + 10
     return number
+
 
 numbers = (result(first_program_output),
            result(second_program_output),
