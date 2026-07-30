ceb1e59eb98ae2333e50bfd1ac43bb310c2ebddb
Dmitriy Fateev <dmitrijfateev82@gmail.com>
2026-07-30 17:32:18 +0700

    Calculation edit for requirements

--- homework/dmitriy_fateev/Lesson_9/Map_filter.py
+++ homework/dmitriy_fateev/Lesson_9/Map_filter.py
@@ -1,9 +1,8 @@
 temperatures = [20, 15, 32, 34, 21, 19, 25, 27, 30, 32, 34, 30, 29, 25, 27,
                 22, 22, 23, 25, 29, 29, 31, 33, 31, 30, 32, 30, 28, 24, 23]
 
-high_temperatures = filter(lambda temp: temp > 28, temperatures)
+high_temperatures = list(filter(lambda temp: temp > 28, temperatures))
 
-print(*list(high_temperatures))
-print(min(temperatures))
-print(max(temperatures))
-print(int(sum(temperatures) / len(temperatures)))
+print(min(high_temperatures))
+print(max(high_temperatures))
+print(int(sum(high_temperatures) / len(high_temperatures)))
