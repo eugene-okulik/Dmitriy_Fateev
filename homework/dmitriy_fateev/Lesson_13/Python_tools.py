6dae91bc1b118f8429ea6a50828679ad31fca622
Dmitriy <dmitrijfateev82@gmail.com>
2026-08-27 00:50:19 +0700

    thirteenth lesson

---
+++ homework/dmitriy_fateev/Lesson_13/Python_tools.py
@@ -0,0 +1,36 @@
+from datetime import datetime, timedelta
+
+NOW = datetime.now()
+DAYS_OF_WEEK = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"]
+
+def read_file():
+    path = "../../eugene_okulik/hw_13/data.txt"
+    with open(path, 'r', encoding='utf-8') as data_file:
+        for line in data_file:
+            yield line[3:28]
+
+LINE = read_file()
+
+def parse_date(line):
+    line = line.strip()
+    return datetime.strptime(line, "%Y-%m-%d %H:%M:%S.%f")
+
+def task_1(date):
+    print(f"1. На неделю позже: {date + timedelta(weeks=1)}")
+
+def task_2(date):
+    print(f"2. День недели: {DAYS_OF_WEEK[date.weekday()]}")
+
+def task_3(date):
+    print(f"3. Было дней назад: {(NOW - date).days}")
+
+STEPS = [task_1, task_2, task_3]
+
+for step_number, step in enumerate(STEPS):
+    this_line = ""
+    try:
+        this_line = next(LINE)
+        step(parse_date(this_line))
+
+    except ValueError as e:
+        print(f"Ошибка на шаге {step_number} (строка: '{this_line}'): {e}")
